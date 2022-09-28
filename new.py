164a349bb5fa84b52663c8961ace98e3a3d14166
manikanta.kg <manikanta.kg@xelpmoc.in>
2022-09-28 10:19:59 +0530

    initial commit

---
+++ new.py
@@ -0,0 +1,38 @@
+from asyncore import loop
+
+
+print("Hello world!!!")
+
+
+a = 5
+
+b = 15
+
+c = a+b
+
+
+print(c)
+
+
+cars = ['asdf', 'dfgh', 'erty']
+
+
+
+
+for x in cars: 
+    print(cars)
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+#--------------------------Loops----------------
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
