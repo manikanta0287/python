e24e77f0a6736e100e78e136a77835f9aa3f3012
manikanta.kg <manikanta.kg@xelpmoc.in>
2022-10-12 12:01:05 +0530

    loops concepts are added separetly

---
+++ loops.py
@@ -0,0 +1,22 @@
+cars = ['asdf', 'dfgh', 'erty']
+
+
+
+
+for x in cars: 
+    print(cars)
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+#--------------------------Loops----------------
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-from asyncore import loop
+#from asyncore import loop
 
 
 print("Hello world!!!")
@@ -14,25 +14,3 @@ c = a+b
 print(c)
 
 
-cars = ['asdf', 'dfgh', 'erty']
-
-
-
-
-for x in cars: 
-    print(cars)
-adj = ["red", "big", "tasty"]
-fruits = ["apple", "banana", "cherry"]
-
-for x in adj:
-  for y in fruits:
-    print(x, y)
-
-#--------------------------Loops----------------
-
-adj = ["red", "big", "tasty"]
-fruits = ["apple", "banana", "cherry"]
-
-for x in adj:
-  for y in fruits:
-    print(x, y)
